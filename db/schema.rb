57f051bbc3cdec462a2e491b3cf9e47d38620207
bergercm <bergercm@umich.edu>
2017-06-28 11:38:47 -0400

    trying to force through schema

--- db/schema.rb
+++ db/schema.rb
@@ -9,8 +9,6 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-#
-# I made a change
 
 ActiveRecord::Schema.define(version: 20170627190035) do
 
