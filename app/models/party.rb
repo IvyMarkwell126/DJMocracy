b838258afff2838453ac4d5bbe6ca316bd08b2d3
bergercm <bergercm@umich.edu>
2017-07-01 13:34:06 -0400

    Added the rails relationships to the models

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,2 +1,7 @@
 class Party < ApplicationRecord
+    has_many :party_songs
+    has_many :songs, through: :party_songs
+
+    has_many :party_users
+    has_many :users, through: :party_users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
     #So rails can return the name of the user when it needs to
+    has_many :party_users
+    has_many :parties, through: :party_users
+
+
     def to_s
         name
     end
