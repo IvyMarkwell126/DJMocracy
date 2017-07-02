d4ec244e2f594059a8142201af926e3647dfe15e
bergercm <bergercm@umich.edu>
2017-07-01 21:04:53 -0400

    added nested routes for user/:id/parties

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,19 @@ Rails.application.routes.draw do
   resources :party_songs
   resources :parties
   resources :songs
-  get 'home/index'
 
-      resources :party
+  #nested users/:id/parties
+  resources :users do
+      resources :parties
+  end
+
+  get 'home/index'
+  
+  #resources :party
     
-    match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
-    match 'auth/failure', to: redirect('/'), via: [:get, :post]
-    match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
+    #match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+    #match 'auth/failure', to: redirect('/'), via: [:get, :post]
+    #match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
 
     root 'home#index'
 end
