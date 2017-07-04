82bc18edffd8b1c302771fd16d24c3bd039e04a8
bergercm <bergercm@umich.edu>
2017-07-04 15:05:53 -0400

    Users can see the parties they belong to

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+      @user = User.find(params[:id])
+      @user_parties = @user.parties
   end
 
   # GET /users/new
