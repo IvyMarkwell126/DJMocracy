53a594f3644fffaa758ca44c18022075d33b5af6
ivymarkwell126 <ivy.markwell@utexas.edu>
2017-07-10 11:11:50 -0400

    Added functionality for creating individual songs.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,12 @@
 class Song < ApplicationRecord
+	def add_song(artist, title)
+		puts song.to_s
+		if Song.where("artist in #{artist}") and Song.where("title in #{title}")
+			return
+		end
+		Song.create({
+			title: title,
+			artist: artist
+		})
+	end	
 end
